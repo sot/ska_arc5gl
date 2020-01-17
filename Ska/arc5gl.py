ab7b7e401f18e5a170104d73598d82197225f376
Javier Gonzalez <javierggt@yahoo.com>
2020-01-17 17:06:05 -0500

    use __name__ instead of __package__ in ska_helpers.get_version

--- Ska/arc5gl.py
+++ Ska/arc5gl.py
@@ -9,7 +9,7 @@ import pexpect
 # Should put in a watchdog timer to exit from arc5gl after a period of inactivity
 import ska_helpers
 
-__version__ = ska_helpers.get_version(__package__)
+__version__ = ska_helpers.get_version(__name__)
 
 
 class Arc5gl(object):
